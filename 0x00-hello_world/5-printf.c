1b69aa808ba00a727a5eb6833cd56265476b6593
Delight_mpashi <delight_mpashi@outlook.com>
2022-10-15 02:24:06 -0700

    this is commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-
-
-
 /**
- *
  * main - Entry point
- *
- *
  *
  *Return: Always 0 (Success)
  *
@@ -18,5 +12,4 @@ int main(void)
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
