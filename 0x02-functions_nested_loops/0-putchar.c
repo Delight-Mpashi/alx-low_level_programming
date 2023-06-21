a11a10608fd43b0a56997124f33e77edb539cac8
Delight C. Mpashi <73102519+Delight-Mpashi@users.noreply.github.com>
2023-06-21 19:36:55 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - prints the string "ALXISCOOl!" from a character array.
+ * main - prints the string "ALXISCOOl" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "ALXISCOOl!";
+	char c[] = "ALXISCOOl";
 	int i;
 
 	for (i = 0; i < 9; i++)
